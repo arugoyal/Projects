58a5ff1754925bbd6f4ea4b490e90d444f1d9d67
aru goyal <arugoyal08@gmail.com>
2019-01-16 17:51:41 +0530

    file added

---
+++ Transport Management System/js/scroll.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+		// scroll body to 0px on click
+		$('#back-top a').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+	});
